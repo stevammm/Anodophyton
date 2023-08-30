b0d4ba9457a48ee972a64d82a1a0f7aeab4c44be
Stevam <129697376+stevammm@users.noreply.github.com>
2023-08-30 20:09:09 -0300

    Update display7seguimentos.py

--- display7seguimentos.py
+++ display7seguimentos.py
@@ -1,1 +1,29 @@
+import utime
+from machine import Pin
 
+leds = [1,2,3,4,5,6,7,8]
+
+nums = [
+    [1,0,0,1,1,1,1,1],
+    [0,0,1,0,0,1,0,1],
+    [0,0,0,0,1,1,0,1],
+    [1,0,0,1,1,0,0,1],
+    [0,1,0,0,1,0,0,1],
+    [0,1,0,0,0,0,0,1],
+    [0,0,0,1,1,1,1,1],
+    [0,0,0,0,0,0,0,1],
+    [0,0,0,0,1,0,0,1],
+    [0,0,0,0,1,0,0,0]
+  ]
+for index in range(8):
+  leds[index] = Pin(leds[index],Pin.OUT)
+
+def ligar(oqueFazer):
+  for index in range(8):
+    leds[index].value(oqueFazer[index])
+
+while True:
+  for index in range(10):
+    ligar(nums[index])
+    utime.sleep(0.7)
+  
